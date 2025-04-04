0472b5df1b4b9603f188c14e95a64f09e0e74aa0
DanielAikhonmu <danielose2009@yahoo.com>
2025-04-03 23:40:48 -0400

    03. update and rename of index.html in exercise folder to exercise.html

--- Intermediate/03. others/Exercise/exercise.js
+++ Intermediate/03. others/Exercise/exercise.js
@@ -2,8 +2,64 @@
 
 // Iterate over object & log the property and the value of that object using for in loop.
 
+/*
 const object = { a: 1, b: 2, c: 3 };
 
 for (const properties in object) {
 	console.log(`${properties}: ${object[properties]}`);
+} 
+*/
+
+// ****for of loop***
+
+/*
+
+const array1 = ["apple", "banana", "cherry"];
+
+for (const elements of array1) {
+	console.log(elements);
 }
+
+*/
+
+/*
+const colors = ["teal", "blue", "red", "green"];
+
+colors.forEach(color => console.log(color));
+*/
+
+
+
+/*
+const myNum = [4, 7, 16, 3, 35, 98, 237, 12, 5, 9786, 4274, 43];
+
+const sumMyNum = myNum.reduce((acc, num) => acc + num, 0);
+
+console.log(sumMyNum);
+*/
+
+
+
+
+// 1. Create an array of numbers.
+// 2. Create a variable to hold the sum.
+// 3. Create the adder function which takes the parameter of   number & add that with the sum.
+// 4. Loop over the array, incrementing the sum variable except this time a function gets referenced in the forEach
+// 5. print the sum variable.
+
+
+
+let myNum = [4, 7, 16, 3, 35];
+
+let sum = 0;
+
+function adder(number) {
+	sum = sum + number;
+}
+
+myNum.forEach(adder);
+console.log(sum);
+
+
+
+
--- Intermediate/03. others/app.js
+++ Intermediate/03. others/app.js
@@ -24,6 +24,7 @@ for (let details in person) {
 
 */
 
+/*
 let list = ["one", 'two', "three", 'four'];
 
 for (let index in list) {
@@ -37,3 +38,61 @@ let number = ["ones's", "two's", "threes's", "four's"];
 for (let numbers in number) {
 	console.log(`${numbers}: ${number[numbers]}`);
 }
+
+*/
+
+// *****for of loop*****
+
+/*
+let people = ["Daniel", "tobi", "Dolapo", "Maria"];
+
+for (let person of people) {
+	console.log(person);
+}
+*/
+
+/*
+const text = "duplicate my time"
+
+for (const char of text) {
+	console.log(char)
+}
+*/
+
+// *****for each method*****
+
+/*
+const colors = ["teal", "blue", "red", "green"];
+
+colors.forEach(color => console.log(color));
+*/
+
+/*
+const words = ["hello", "bird", "table", "football", "pipe", "code"];
+
+const capWords = words.forEach((word, index, array) => {
+	array[index] = word[0].toUpperCase() + word.substring(1);
+});
+
+console.log(words);
+*/
+
+// The .forEach() method does not return a new array, meaning capWords will be undefined.
+
+//However, since forEach() modifies the original array in place, console.log(words) 
+// will still 
+// print the modified array.
+
+//.forEach(): Modifies the original array but does not return a new one.
+
+//.map(): Creates and returns a new transformed array.
+
+/*
+
+const words = ["hello", "bird", "table", "football", "pipe", "code"];
+
+const capWords = words.map(word => word[0].toUpperCase() + word.substring(1));
+
+console.log(capWords);
+
+*/
