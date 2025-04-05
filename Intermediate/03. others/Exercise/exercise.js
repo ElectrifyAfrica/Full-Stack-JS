184fe64988846397b84fe7a2b598081271d1754b
DanielAikhonmu <danielose2009@yahoo.com>
2025-04-05 03:19:30 -0400

    03. update with more methods e.g, some, every ....

--- Intermediate/03. others/Exercise/exercise.js
+++ Intermediate/03. others/Exercise/exercise.js
@@ -48,7 +48,7 @@ console.log(sumMyNum);
 // 5. print the sum variable.
 
 
-
+/*
 let myNum = [4, 7, 16, 3, 35];
 
 let sum = 0;
@@ -59,7 +59,209 @@ function adder(number) {
 
 myNum.forEach(adder);
 console.log(sum);
+*/
+
+/*
+const numbers = [10, 3, 5, 8, 20];
+
+let compounding = numbers.map(num => num * 10);
+
+console.log (compounding);
+*/
+
+/*
+const numbers1 = [10, 3, 5, 8, 20];
+
+function myFunction(num) {
+	return num * 100; 
+}
+
+const newArr = numbers1.map(myFunction)
+console.log(newArr);
+*/
+
+/*
+//****array filter ****
+
+// 1. Iterate over "ages" array
+// 2. Print only adults, those whos age is greater then 18
+
+const ages = [32, 33, 16, 17, 40];
+
+greaterAges = ages.filter(age => age > 18);
+
+console.log(greaterAges);
+
+
+//sol 2
+
+const ages1 = [37, 50, 16, 22, 4, 12, 40];
+
+function checkAdult(age) {
+  return age >= 18;
+}
+
+const res = ages1.filter(checkAdult);
+console.log(res);
 
 
 
+// 1. Iterate Over "words" array
+// 2. Print only those words which length is greater then 6
 
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
+
+words.forEach((word) => {
+	console.log(word.length)
+});
+
+//console.log(words.filter(word1 => word1.length > 6));
+
+const result = words.filter(word => word.length > 6);
+console.log(result);
+
+*/
+
+/*
+let products1 = [
+	{ name: "Checkers", category: "Toys" },
+	{ name: "Harry Potter", category: "Books" },
+  { name: "iPhone", category: "Electronics" },
+  { name: "Learn PHP", category: "Books" },
+];
+
+const productsRes1 = products1.filter(product1 => product1.category === "Books");
+
+console.log(productsRes1);
+*/
+
+//***find method***
+
+// -------------------------------
+// 1. Iterate over "ages" array
+// 2. Print only those ages which is greater then 18
+
+/*
+const ages = [3, 10, 18, 23, 24];
+
+const agesRes = ages.find(age => age > 18);
+
+console.log(agesRes);
+
+
+//2nd solution
+
+const age1 = [3, 10, 18, 20, 67];
+
+function checkAge(age) {
+	return age > 18;
+}
+
+const res = age1.find(checkAge);
+
+console.log(res);
+*/
+
+// -------------------------------
+// BONUS => REFACTOR
+// let products = [
+//   { name: "Checkers", category: "Toys" },
+//   { name: "Harry Potter", category: "Books" },
+//   { name: "iPhone", category: "Electronics" },
+//   { name: "Learn PHP", category: "Books" },
+// ];
+// let product;
+
+// for (let i = 0; i < products.length; i++) {
+//   if (products[i].category === "Books") {
+//     product = products[i];
+//     break;
+//   }
+// }
+// console.log(product);
+
+/*
+let products = [
+	{ name: "Checkers", category: "Toys" },
+	{ name: "Harry Potter", category: "Books" },
+  { name: "iPhone", category: "Electronics" },
+  { name: "Learn PHP", category: "Books" },
+];
+
+const productsRes = products.find(product => product.category === "Books");
+
+console.log(productsRes);
+*/
+
+// *****every and some helper****
+/*
+// REFACTOR -> USE EVERY & SOME HELPERS
+let products = [
+  { name: "Checkers", category: "Toys" },
+  { name: "Harry Potter", category: "Books" },
+  { name: "iPhone", category: "Electronics" },
+  { name: "Learn PHP", category: "Books" },
+];
+
+let allProductsBooks = true;
+let someProductsBooks = false;
+
+for (let i = 0; i < products.length; i++) {
+  if (products[i].category != "Books") {
+    allProductsBooks = false;
+  } else {
+    someProductsBooks = true;
+  }
+}
+console.log(allProductsBooks);
+console.log(someProductsBooks);
+
+
+//my solution
+
+let products1 = [
+  { name: "Checkers", category: "Toys" },
+  { name: "Harry Potter", category: "Books" },
+  { name: "iPhone", category: "Electronics" },
+  { name: "Learn PHP", category: "Books" },
+];
+
+const everyPrdRes = products1.every(product => product.category === "Books");
+const somePrdRes = products1.some(product => product.category === "Books");
+
+console.log(everyPrdRes);
+console.log(somePrdRes);
+*/
+
+// their solution
+
+/*
+let products = [
+  { name: "Checkers", category: "Toys" },
+  { name: "Harry Potter", category: "Books" },
+  { name: "iPhone", category: "Electronics" },
+  { name: "Learn PHP", category: "Books" },
+];
+
+
+// do all products have a category of Books
+let allProductsBooks = products.every(function (product) {
+  return product.category === "Books";
+});
+
+// do any products have a category of Books
+let someProductsBooks = products.some(function (product) {
+  return product.category === "Books";
+});
+
+console.log(allProductsBooks);
+console.log(someProductsBooks);
+
+*/
--- Intermediate/03. others/app.js
+++ Intermediate/03. others/app.js
@@ -68,6 +68,15 @@ colors.forEach(color => console.log(color));
 */
 
 /*
+let numbers = [1, 2, 3, 4, 5, 6];
+
+numbers.forEach(function(number) {
+  console.log(number * 2);
+});
+*/
+
+/*
+
 const words = ["hello", "bird", "table", "football", "pipe", "code"];
 
 const capWords = words.forEach((word, index, array) => {
@@ -75,7 +84,9 @@ const capWords = words.forEach((word, index, array) => {
 });
 
 console.log(words);
+
 */
+
 
 // The .forEach() method does not return a new array, meaning capWords will be undefined.
 
@@ -95,4 +106,130 @@ const capWords = words.map(word => word[0].toUpperCase() + word.substring(1));
 
 console.log(capWords);
 
+*/
+
+/*
+let numbers = [1, 2, 3, 4, 5, 6];
+
+let double = numbers.map(num => num * 2);
+
+console.log(double);
+*/
+
+/*
+let peoples = [
+	{firstName: "Daniel", lastName: "Aikhonmu"},
+	{firstName: "Tobi", lastName: "Ogunyele"},
+	{firstName: "Zalika", lastName: "Scott"}
+];
+
+const results = peoples.map(person => {
+	return [person.firstName, person.lastName]
+})
+
+console.log(results);
+*/
+
+
+/*
+// The filter() method is a built-in array method in JavaScript that allows you to create a new array containing elements that pass a certain condition. It provides a clean and concise way to filter out elements from an array based on a specified criteria.
+
+const songs = [
+  { name: "Lucky You", duration: 4.34 },
+  { name: "Just Like You", duration: 3.23 },
+  { name: "The Search", duration: 2.33 },
+  { name: "Old Town Road", duration: 1.43 },
+  { name: "The Box", duration: 5.23 },
+];
+
+// We take an element from our source array & we pass it into our iterator function
+// --> that iterator function has to return either a truthy or falsy value
+// --> if it returns truthy value it will be included in a result array
+// --> if it returns falsy value however, this new value will not be included in the new array
+
+console.log(songs.filter(songs => songs.duration > 3));
+
+
+const computers = [
+	{ ram:4, hdd: 100 },
+	{ ram:8, hdd: 100 },
+	{ ram:16, hdd: 100 },
+	{ ram:32, hdd: 100 },
+	{ ram:64, hdd: 100 },
+];
+
+console.log(computers.filter(comp => comp.ram <= 16));
+*/
+
+
+/*
+// The find() method is another built-in array helper in JavaScript that allows you to find the first element in an array that matches a specific condition. It returns the value of the first element that satisfies the given testing function, or undefined if no element is found.
+
+// ------------------------------
+
+const peoples = [
+  { name: "huxn", age: 17 },
+  { name: "john", age: 18 },
+  { name: "alex", age: 20 },
+  { name: "jimmy", age: 30 },
+  { name: "alex", age: 30 },
+];
+
+
+const res = peoples.find(person => person.name === "alex");
+console.log(res);
+
+
+// 2nd example
+
+const posts = [
+  { id: 1, content: "Good Post" },
+  { id: 2, content: "Funny Post" },
+  { id: 3, content: "Sad Post" },
+	{ id: 4, content: "Funny Post" },
+];
+
+const postRes = posts.find(post => post.content === "Funny Post");
+console.log(postRes); 
+*/
+
+
+// *****every and some helper****
+//////////////////////////////////////////
+// The every() and some() are two built-in array methods in JavaScript that allow you to check the elements of an array based on a given condition. Both methods take a callback function as an argument, which is applied to each element of the array.
+
+// The every() method tests whether all elements in the array pass the condition specified by the provided callback function. It returns true if the callback function returns true for every element, and false if any element fails the condition.
+
+// The some() method tests whether at least one element in the array passes the condition specified by the provided callback function. It returns true if the callback function returns true for at least one element, and false if no element passes the condition.
+
+//////////////////////////////////////////
+
+/*
+const peoples = ["huxn", "jordan", "alex"];
+
+const res = peoples.every(people => people.length === 4);
+const res1 = peoples.some(people => people.length === 4);
+
+
+console.log(res);
+console.log(res1);
+
+console.log("");
+
+const songs = [
+  { name: "Lucky You", duration: 4.34 },
+  { name: "Just Like You", duration: 3.23 },
+  { name: "The Search", duration: 2.33 },
+  { name: "Old Town Road", duration: 1.43 },
+  { name: "The Box", duration: 5.23 },
+];
+
+const everyRes = songs.every(song => song.duration > 3);
+const someRes = songs.some(song => song.duration > 3);
+const everyRes1 = songs.every(song => song.duration > 1);
+
+
+console.log(everyRes);
+console.log(someRes);
+console.log(everyRes1);
 */
