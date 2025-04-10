d529bc65e8b24ec3925dfb13ed7c98ba7479127e
DanielAikhonmu <danielose2009@yahoo.com>
2025-04-10 03:07:44 -0400

    DOM starter continued

---
+++ DOM/Starter/2. Accessing the DOM/live-coding/app.js
@@ -0,0 +1,16 @@
+// ************ DOM SELECTORS ************
+// 1. getElementsByTagName
+//console.log(document.getElementsByTagName("h1"));
+//console.log(document.getElementsByTagName("h1").length);
+
+
+// 2. getElementById
+
+
+
+// 3. getElementsByClassName
+// 4. querySelector
+// 5. querySelectorAll
+
+// ---------------------------------------
+// Storing data in variables
