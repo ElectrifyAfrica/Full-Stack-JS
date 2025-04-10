f9664d758f2c769e0fdeb172f3fac25ed96787f0
DanielAikhonmu <danielose2009@yahoo.com>
2025-04-10 04:15:27 -0400

    Accessing Element using the DOM

---
+++ DOM/Starter/2. Accessing the DOM/exercise/app.js
@@ -0,0 +1,28 @@
+// 1. Select element h4 which holds text of "Red" by using getElementsByTagName
+
+const h4 = document.getElementsByTagName("h4");
+console.log(h4);
+
+// 2. Select div with the class of "green" which holds text (Green) by using getElementsByClassName
+
+const greenDiv = document.getElementsByClassName("green");
+console.log(greenDiv);
+
+
+
+// 3. Select div with the ID of blue which holds the text "Blue" by using getElementById
+
+const blue = document.getElementById("blue");
+console.log(blue);
+
+// 4. Select div which has the class & Id of "yello" by using querySelector()
+const yellowClass = document.querySelector(".yellow");
+console.log(yellowClass);
+
+const yellowId = document.querySelector("#yellow");
+console.log(yellowId);
+
+// 5. Select all the elements which has the class of "teal" by using querySelectorAll
+
+const allTeal = document.querySelectorAll(".teal");
+console.log(allTeal);
--- DOM/Starter/2. Accessing the DOM/live-coding/app.js
+++ DOM/Starter/2. Accessing the DOM/live-coding/app.js
@@ -5,12 +5,33 @@
 
 
 // 2. getElementById
-
-
+//console.log(document.getElementById("main"));
+//console.log(document.getElementById("id-1"));
 
 // 3. getElementsByClassName
+//console.log(document.getElementsByClassName(""));
+//console.log(document.getElementsByClassName("cls"));
+//console.log(document.getElementsByClassName("cls-1"));
+
+
 // 4. querySelector
+//console.log(document.querySelector("h1")); // by tag name
+//console.log(document.querySelector(".cls")); //by class name
+//console.log(document.querySelector(".danny")); //null
+//console.log(document.querySelector("cls")); //null
+//console.log(document.querySelector("#id-1")); // by Id
+//console.log(document.querySelector("li"));
+
+
 // 5. querySelectorAll
+//console.log(document.querySelectorAll(".cls"));
+//console.log(document.querySelectorAll(".cls").length);
+//console.log(document.querySelectorAll("li"));
 
 // ---------------------------------------
 // Storing data in variables
+const h1 = document.querySelector("h1");
+console.log(h1);
+
+const li = document.querySelectorAll("li");
+console.log(li);
