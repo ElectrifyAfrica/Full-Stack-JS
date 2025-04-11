eceda7199fabfe2fc55eae0d52e87e1c87d78087
DanielAikhonmu <danielose2009@yahoo.com>
2025-04-11 05:51:22 -0400

    03 innerText, TextContent, innerText

---
+++ DOM/Starter/3. innerText, textContent, innerHTML/exercise/app.js
@@ -0,0 +1,19 @@
+// 1. Check the innerText of "first" element
+
+const h1first = document.querySelector(".first");
+console.log(h1first.innerText);
+
+
+// 2. Check the textContent of "second" element
+
+const h1second = document.querySelector(".second");
+console.log(h1second.textContent);
+
+
+
+// 3. Check the innerHTML of "third" element
+
+const h1third = document.querySelector(".third");
+console.log(h1third.innerHTML);
+//console.log(h1third.textContent);
+//console.log(h1third.innerText);
---
+++ DOM/Starter/3. innerText, textContent, innerHTML/live coding/app.js
@@ -0,0 +1,21 @@
+// innerText
+//const p = document.querySelector("p");
+//console.log(p.innerText);
+
+// textContent
+//console.log(p.textContent);
+
+
+
+// innerHTML
+
+//console.log(p.innerHTML);
+
+
+const h1 = document.querySelector("h1");
+//h1.innerText = "Text Changed";
+//h1.innerHTML = "<em>Daniel Is Great</em>";
+h1.innerHTML = "<del>Mistake</del>";
+
+
+
