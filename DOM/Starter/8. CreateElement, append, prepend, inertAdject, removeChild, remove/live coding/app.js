02724d9e036f7903c2b15f9fd8dd068ef0f36851
DanielAikhonmu <danielose2009@yahoo.com>
2025-04-13 22:07:19 -0400

    8. element append, create, inertAdject, removeChild, remove completed.

--- DOM/Starter/8. CreateElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
+++ DOM/Starter/8. CreateElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
@@ -1,9 +1,51 @@
 // ------------------------------------
+
+/*
 // createElement()
+const h1 = document.createElement("h1");
+//console.log(h1);
+h1.textContent = "Goodbye";
+//console.log(h1.textContent);
+
+h1.classList.add("greetings");
+
+//h1.innerHTML= "Hello World111";
+
+console.log(h1);
+
+
+
 // appendChild()
-// append()
-// prepend()
+const body = document.body;
+body.appendChild(h1);
+*/
+
+/*
+//const ul = document.querySelector("ul");
+
+//const newLi = document.createElement("li");
+//newLi.innerText = "I'm li Tag1"
+//ul.appendChild(newLi);
+
+
 // insertBefore()
+
+//const newestLi = document.createElement("li");
+//newestLi.innerText = "I'm li Tag2"
+//const firstLi = document.querySelector("li");
+// selector.insertBefore(what, where)
+//ul.insertBefore(newestLi, firstLi);
+
+
+
+
+//insert adjacent()
+
+//const firstP = document.querySelector("p");
+//const i = document.createElement("i");
+//i.innerText = "I'm italics";
+//i.style.color = "skyblue";
+*/
 
 /* insertAdjacentElement(
     'beforebegin'
@@ -13,6 +55,51 @@
     element
 ) */
 
+
+//firstP.insertAdjacentElement("beforebegin", i);
+
+//firstP.insertAdjacentElement("afterbegin", i);
+
+//firstP.insertAdjacentElement("beforeend", i);
+
+//firstP.insertAdjacentElement("afterend", i);
+
+/*
+// append()
+let section = document.querySelector("section");
+
+const i = document.createElement("i");
+i.innerText = "I'm italics";
+i.style.color = "skyblue";
+
+const span = document.createElement("span");
+span.innerText = "Danny2";
+span.style.color = "teal";
+
+const h4 = document.createElement("h4");
+h4.innerText = "Danny";
+h4.style.color = "crimson";
+
+
+
+
+//section.append(i, span, h4);
+
+
+// prepend()
+
+section.prepend(i, span, h4);
+*/
+
+
+
 // removeChild()
 // remove()
+/*
+const newList = document.querySelector(".new-list");
+const fourth = document.querySelector(".fourth");
+newList.removeChild(fourth);
+//newList.remove();
+*/
+
 // ------------------------------------
